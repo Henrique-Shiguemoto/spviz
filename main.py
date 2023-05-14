396fcfac9b8b08f25472e1fb84ce64cf8e8c69bc
Rick Shiguemoto <shiguemoto88@gmail.com>
2023-05-13 23:02:11 -0300

    Added a bar chart showing the average ratings per season

--- main.py
+++ main.py
@@ -4,28 +4,42 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.read_csv("data/sp_ratings.csv")
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+# creating a new column to hold average ratings for season (yes, duplicated values)
+df['avg_season_rating'] = [0]*len(df)
+
+for i in range(np.max(df['season_number'])):
+    season_subset = df[df['season_number'] == i + 1]
+    season_ratings = season_subset['rating']
+    average_season_rating = np.mean(season_ratings)
+    df.loc[df['season_number'] == i + 1, 'avg_season_rating'] = average_season_rating
+
+# This only has 21 elements whereas df['avg_season_rating'] has 200+ elements
+avg_ratings = df['avg_season_rating'].unique()
+
+# Creating the bar chart to be displayed with dcc in the layout
+x_range = range(1,22)
+fig = px.bar(df, x=x_range, 
+                 y=avg_ratings, 
+                 barmode="group", 
+                 labels={"x" : "Season", "y" : "Average Rating"}, 
+                 color=x_range)
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
+    html.H1(children='South Park Data Visualization'),
     html.Div(children='''
-        Dash: A web application framework for your data.
+        This app shows ratings for each season of South Park (from the 1st to 21st).
     '''),
 
     dcc.Graph(
-        id='example-graph',
+        id='season-ratings-bar-chart',
         figure=fig
     )
 ])
